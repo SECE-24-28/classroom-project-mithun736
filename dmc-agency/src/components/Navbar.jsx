f4f92628fe086bc72974fa9161955ccbded1c8c6
mithun736 <mithunrohith@gmail.com>
2025-12-09 10:23:38 +0530

    commit

---
+++ dmc-agency/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav className="dmc-navbar">
+      <div className="dmc-logo">Devil May Cry Agency</div>
+
+      <ul className="dmc-menu">
+        <li>Home</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
